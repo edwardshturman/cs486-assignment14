0606461e37b6a4253e330366d7c00a03cb72235e
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-05-03 21:58:33 -0700

    Fix make zsh incompatibility

--- make.sh
+++ make.sh
@@ -15,10 +15,10 @@ PROJECT_DIR="$(cd "$(dirname "$0")"; pwd)"
 export AWS_PROFILE AWS_REGION PROJECT_NAME PROJECT_DIR
 
 
-log()   { echo -e "\e[30;47m ${1^^} \e[0m ${@:2}"; }        # $1 uppercase background white
-info()  { echo -e "\e[48;5;28m ${1^^} \e[0m ${@:2}"; }      # $1 uppercase background green
-warn()  { echo -e "\e[48;5;202m ${1^^} \e[0m ${@:2}" >&2; } # $1 uppercase background orange
-error() { echo -e "\e[48;5;196m ${1^^} \e[0m ${@:2}" >&2; } # $1 uppercase background red
+log()   { echo -e "\e[30;47m $(echo $1 | tr '[:lower:]' '[:upper:]') \e[0m ${@:2}"; }        # $1 uppercase background white
+info()  { echo -e "\e[48;5;28m $(echo $1 | tr '[:lower:]' '[:upper:]') \e[0m ${@:2}"; }      # $1 uppercase background green
+warn()  { echo -e "\e[48;5;202m $(echo $1 | tr '[:lower:]' '[:upper:]') \e[0m ${@:2}" >&2; } # $1 uppercase background orange
+error() { echo -e "\e[48;5;196m $(echo $1 | tr '[:lower:]' '[:upper:]') \e[0m ${@:2}" >&2; } # $1 uppercase background red
 
 # https://unix.stackexchange.com/a/22867
 export -f log info warn error
